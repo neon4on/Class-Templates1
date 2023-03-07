a50218f1abce2aeac051f6d201bf85409bf405de
Valeriy Kamenskikh <86181396+neon4on@users.noreply.github.com>
2023-03-07 13:49:59 +0400

    Add files via upload

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -21,6 +21,8 @@ namespace ConsoleApp1
                 Console.WriteLine("Заявки на авиабилеты: \n\n");
                 Console.WriteLine("1 - Добавление новой заявки \n");
                 Console.WriteLine("2 - Вывод всех заявок на экран \n");
+                Console.WriteLine("3 - Вывод заявок по номер заявки рейса \n");
+                Console.WriteLine("4 - Удаление заявок по номеру рейса \n");
                 Console.WriteLine("5 - Выход из программы \n");
                 Console.WriteLine("ВЫБОР: ");
                 select = Convert.ToInt32(Console.ReadLine());
@@ -32,16 +34,14 @@ namespace ConsoleApp1
         public class Avia : IEquatable<Avia>
         {
             public string target { get; set; } //Пункт назначения
-            public string number { get; set; }//Номер рейса
             public string fio { get; set; } //ФИО:
             public string uwant { get; set; } //Желаемая дата вылета
-            public string PartName { get; set; }
 
-            public int PartId { get; set; }
+            public int PartId { get; set; } //Номер рейса
 
             public override string ToString()
             {
-                return "ID: " + target + "   Name: " + number + "ID: " + target + "   Name: " + number;
+                return "Пункт назначения: " + target + ";  Номер рейса: " + PartId + ";  ФИО: " + fio + ";  Желаемая дата вылета: " + uwant;
             }
             public override bool Equals(object obj)
             {
@@ -65,79 +65,72 @@ namespace ConsoleApp1
             {
                 // В односвязанный список необходимо объект в котором будет несколько полей
                 List<Avia> parts = new List<Avia>();
-                parts.Add(new Avia() { PartName = "crank arm", PartId = 1234 });
-                parts.Add(new Avia() { PartName = "chain ring", PartId = 1334 });
-                parts.Add(new Avia() { PartName = "regular seat", PartId = 1434 });
-                parts.Add(new Avia() { PartName = "banana seat", PartId = 1444 });
-                parts.Add(new Avia() { PartName = "cassette", PartId = 1534 });
-                parts.Add(new Avia() { PartName = "shift lever", PartId = 1634 });
-                parts.Add(new Avia() { target = "shift lever", number = "55", fio = "1634", uwant = "4829" });
+                parts.Add(new Avia() { target = "Ульяновск", PartId = 5 , fio = "Каменских", uwant = "Казань" });
+                string a, c, d;
+                int b;
+
                 
-                Console.WriteLine();
-                foreach (Avia aPart in parts)
-                {
-                    Console.WriteLine(aPart);
-                }
-                Console.ReadLine();
 
                 //------------------------------------------------------------
 
-                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.ForegroundColor = ConsoleColor.Green;
 
-                //int select;
-                //do
-                //{
-                //    // вызываем главное меню и выполняем выбор пользователя
-                //    select = Menu();
-                //    switch (select)
-                //    {
-                //        case 1:
-                //            {
-                //                Console.Write("Пункт назначения: ");
-                //                person1.target = Console.ReadLine();
-                //                Console.Write("Номер рейса: ");
-                //                person1.number = Console.ReadLine();
-                //                Console.Write("ФИО: ");
-                //                person1.fio = Console.ReadLine();
-                //                Console.Write("Желаемая дата вылета: ");
-                //                person1.uwant = Console.ReadLine();
-                //                List1.Add(person1);
-                //                Console.ReadLine();
-                //                break;
-                //            }
-                //        case 2:
-                //            {
-                //                int i = 1;
-                //                Console.WriteLine("\n");
-                //                foreach (var item in List1)
-                //                {
-                //                    Console.Write($"{i}" + "\t");
-                //                    Console.Write(person1.target + " ");
-                //                    Console.Write(person1.fio + " ");
-                //                    Console.Write(person1.number + " ");
-                //                    Console.Write(person1.uwant + " ");
-                //                    Console.WriteLine("\n");
-                //                    i++;
-                //                }
-                //                Console.ReadLine();
-                //                break;
-                //            }
-                //        case 3:
-                //            {
-                //                //Вывод заявок по номеру рейса
-                //                Console.WriteLine("\n");
-                //                Console.ReadLine();
-                //                break;
-                //            }
-                //        case 4:
-                //            {
-                //                //Удаление заявок по номеру рейса
-                //                Console.WriteLine("\n");
-                //                Console.ReadLine();
-                //                break;
-                //            }
-                //    }
-                //} while (select != 5);
+                int select;
+                do
+                {
+                    // вызываем главное меню и выполняем выбор пользователя
+                    select = Menu();
+                    switch (select)
+                    {
+                        case 1:
+                            {
+                                Console.Write("Пункт назначения: ");
+                                a = Console.ReadLine();
+                                Console.Write("Номер рейса: ");
+                                b = Convert.ToInt32(Console.ReadLine());
+                                Console.Write("ФИО: ");
+                                c = Console.ReadLine();
+                                Console.Write("Желаемая дата вылета: ");
+                                d = Console.ReadLine();
+                                parts.Add(new Avia() { target = a, PartId = b, fio = c, uwant = d });
+                                Console.ReadLine();
+                                break;
+                            }
+                        case 2:
+                            {
+                                int i = 1;
+                                Console.WriteLine("\n");
+                                foreach (Avia aPart in parts)
+                                {
+                                    Console.Write($"{i}" + "\t");
+                                    Console.WriteLine(aPart);
+                                    i++;
+                                }
+                                Console.ReadLine();
+                                break;
+                            }
+                        case 3:
+                            {
+                                //Вывод заявок по номеру рейса
+                                Console.WriteLine("Введите номер заявки, который необходимо вывести: ");
+                                b = Convert.ToInt32(Console.ReadLine()) - 1;
+                                var firstElement = parts[b];
+                                Console.WriteLine(firstElement);
+                                Console.ReadLine();
+                                break;
+                            }
+                        case 4:
+                            {
+                                //Удаление заявок по номеру рейса
+                                Console.WriteLine("Введите номер рейса, который необходимо удалить: ");
+                                b = Convert.ToInt32(Console.ReadLine());
+                                parts.Remove(new Avia() { PartId = b});
+                                Console.WriteLine("\n");
+                                Console.ReadLine();
+                                break;
+                            }
+                    }
+                } while (select != 5);
             }
         }
     }
